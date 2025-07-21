7358880642272c5119186926856fade80db0ed6e
GrzegorzTreter <107022065+g7eg@users.noreply.github.com>
2025-07-21 14:29:59 +0200

    Update l8_z3.py

--- l8/l8_z3.py
+++ l8/l8_z3.py
@@ -1,3 +1,4 @@
+# [2 pkt]
 # Lista 8 Zad.3 
 # Napisać program wyświetlający liczby całkowite z przedziału <x,y> (liczby całkowite x i y podajeużytkownik). W przypadku podania niewłaściwej wartości wyświetl komunikat: "Błąd: Liczba x musi być mniejsza lub równa liczbie y."
 # > Przykład:
