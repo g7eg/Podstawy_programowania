ed5c88b72ae8e2f042ae91e89ff7492f69928014
GrzegorzTreter <107022065+g7eg@users.noreply.github.com>
2025-07-21 14:29:46 +0200

    Update l8_z2.py

--- l8/l8_z2.py
+++ l8/l8_z2.py
@@ -1,3 +1,4 @@
+# [1 pkt]
 # Lista 8 Zad. 2
 # Napisać program wyświetlający liczby całkowite z przedziału <0,y> (liczbę całkowitą y podaje użytkownik). 
 # W przypadku podania niewłaściwej wartości wyświetl komunikat: "Błąd: Liczba y musi być większa lub równa 0."
